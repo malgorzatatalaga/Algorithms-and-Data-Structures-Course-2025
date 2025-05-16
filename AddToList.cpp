a5ab977b4f61d231bb07d53048edb0a956fbdd41
malgorzatatalaga <malgorzatatalaga32@gmail.com>
2025-05-16 21:43:37 +0200

    rand number generator

--- AddToList.cpp
+++ AddToList.cpp
@@ -2,19 +2,36 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <chrono>
 #include "LinkedList.h"
 
 using namespace std;
+using namespace std::chrono;
 
 int main()
 {
     LinkedList linkedList;
-    vector<int> values = {7, 3, 10, 5, 8, 1};
-    
-    for(int v: values)
+    mt19937 gen(random_device{}());
+    vector<int> sizes = {1'000, 2'000, 3'000, 4'000, 5'000, 6'000, 7'000, 8'000, 9'000, 10'000};
+
+    for (int size : sizes)
     {
-        linkedList.addNode(v);
+        vector<int> values(size);
+        iota(values.begin(), values.end(), 1);
+        shuffle(values.begin(), values.end(), gen);
+
+        LinkedList linkedList;
+
+        auto start = high_resolution_clock::now();
+
+        for (int v : values)
+        {
+            linkedList.addNode(v);
+        }
+
+        auto end = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(end - start).count();
+
+        cout << duration << endl;
     }
-    
-    linkedList.printList();
 }
